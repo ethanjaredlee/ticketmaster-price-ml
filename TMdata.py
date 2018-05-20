9f9b9bcdfd4a9a7a43b1253fc9ac74695b1c390d
rawitscherr <rrawitscher101@gmail.com>
2018-05-19 20:27:56 -0500

    fin date

--- TMdata.py
+++ TMdata.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import datetime
+
 apikey = 'sPYngrqc3a29GkMAd2SOBDuPm7VdHT9o'
 headers={'key':apikey}
 
@@ -12,8 +13,24 @@ resp=requests.get('https://app.ticketmaster.com/discovery/v2/events.json?size=20
 response=resp.text
 jsonresp = resp.json()
 for i in range(0,len(jsonresp.get(u'_embedded').get(u'events'))):
+    year=[]
+    month=[]
+    day=[]
     for j in range(0,len(jsonresp.get(u'_embedded').get(u'events')[i].get(u'dates').get(u'start').get(u'localDate'))):
-        
+        if j<=3:
+            year.append(jsonresp.get(u'_embedded').get(u'events')[i].get(u'dates').get(u'start').get(u'localDate')[j])
+        elif j>=5 and j<=6:
+            month.append(jsonresp.get(u'_embedded').get(u'events')[i].get(u'dates').get(u'start').get(u'localDate')[j])
+        elif j>7 and j<=9:
+            day.append(jsonresp.get(u'_embedded').get(u'events')[i].get(u'dates').get(u'start').get(u'localDate')[j])
+
+    dday = day[0]+ day[1]
+    mmonth = month[0] + month[1]
+    yyear = year[0] + year[1] + year[2] + year[3]
+    d=datetime.date(int(yyear),int(mmonth),int(dday))
+    print d.weekday()
+        #print jsonresp.get(u'_embedded').get(u'events')[i].get(u'dates').get(u'start').get(u'localDate')[j]
+
 
 with open('data.txt', 'w') as outfile:
     json.dump(jsonresp, outfile)
