b18979dfd6dee446eb4d2717556a20c5961d74bf
rawitscherr <rrawitscher101@gmail.com>
2018-05-21 16:10:17 -0500

    updated the code for all fields

--- TMdata.py
+++ TMdata.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import datetime
+from event import Event
 
 apikey = 'sPYngrqc3a29GkMAd2SOBDuPm7VdHT9o'
 headers={'key':apikey}
@@ -13,6 +14,7 @@ resp=requests.get('https://app.ticketmaster.com/discovery/v2/events.json?size=20
 response=resp.text
 jsonresp = resp.json()
 for i in range(0,len(jsonresp.get(u'_embedded').get(u'events'))):
+    show=Event()
     year=[]
     month=[]
     day=[]
@@ -28,8 +30,24 @@ for i in range(0,len(jsonresp.get(u'_embedded').get(u'events'))):
     mmonth = month[0] + month[1]
     yyear = year[0] + year[1] + year[2] + year[3]
     d=datetime.date(int(yyear),int(mmonth),int(dday))
-    print d.weekday()
-        #print jsonresp.get(u'_embedded').get(u'events')[i].get(u'dates').get(u'start').get(u'localDate')[j]
+    eventdaynum=d.weekday()
+    weekend=0
+    if eventdaynum==0 or eventdaynum>=5:
+        weekend=1
+    eventcity=jsonresp.get(u'_embedded').get(u'events')[i].get(u'_embedded').get(u'venues')[0].get(u'city').get(u'name')
+    eventvenue= jsonresp.get(u'_embedded').get(u'events')[i].get(u'_embedded').get(u'venues')[0].get(u'name')
+    #pricerange=jsonresp.get(u'_embedded').get(u'events')[i].get(u'priceRanges')
+    artistname=jsonresp.get(u'_embedded').get(u'events')[i].get(u'name')
+    eventid= jsonresp.get(u'_embedded').get(u'events')[i].get(u'_embedded').get(u'attractions')[0].get(u'id')
+    eventgenre= jsonresp.get(u'_embedded').get(u'events')[i].get(u'classifications')[0].get(u'genre').get(u'name')
+    #print eventdaynum,eventid,eventgenre
+    show.weekend=weekend
+    show.artist=artistname
+    show.month=mmonth
+    show.genre=eventgenre
+    show.venue=eventvenue
+    show.city=eventcity
+    print 'NEW EVENT!!''weekend:', show.weekend, 'artist:', show.artist, 'genre:', show.genre,'venue:',show.venue,'city:',show.city,'month:',show.month
 
 
 with open('data.txt', 'w') as outfile:
---
+++ event.py
@@ -0,0 +1,8 @@
+class Event:
+    def __init__(self):
+        self.venue = None
+        self.city = None
+        self.artist= None
+        self.genre=None
+        self.month=None
+        self.weekend=None
